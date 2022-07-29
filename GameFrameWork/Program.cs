02c9aa4b3d4f682007a2b489f8d782fa810e49fa
jongs96 <jjong9696@gmail.com>
2022-07-29 09:21:35 +0900

    0729

--- GameFrameWork/Program.cs
+++ GameFrameWork/Program.cs
@@ -14,7 +14,8 @@ namespace GameFrameWork
         //public double sec_check = 0; //game frame work 내에 멤버변수 생성하여 일처리
         //public int sec = 0;
         double playTime = 0.0;
-        int Count = 0;
+        int s = 0, m = 0, h = 0;
+        bool stop = true;
         public void Start()//반복전에 처음 시작할 때만 하는 초기작업.
         {
             Console.WriteLine("게임을 시작 합니다.");
@@ -23,12 +24,38 @@ namespace GameFrameWork
         {
             if (Console.KeyAvailable)//key를 누르지 않으면 false
             {//key입력이 들어왔을 때만 해당하게 안해주면 응답 대기상태에 빠져버림.
+                ConsoleKey pressedKey = Console.ReadKey(true).Key;
+                //사용한 키를 제거하지 않게 하기위해
+                /*
                 if (Console.ReadKey(true).Key == ConsoleKey.Escape)//key값을 가지고 어떤 키를 입력받았는지 확인
                 {//readkey default 값 false >> 입력한 키 알려줌.
-                    //esc 키를 눌렀으면 false
+                 //esc 키를 눌렀으면 false
                     IsPlaying = false;
                 }
-            }
+                else if(Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+                {
+
+                }
+                */
+                while (true)
+                {
+                    switch (pressedKey)
+                    {
+                        case ConsoleKey.Escape:
+                            IsPlaying = false;
+                            break;
+                        case ConsoleKey.Spacebar:
+                            stop = true;
+                            break;
+                        case ConsoleKey.Enter:
+                            h = 0;
+                            m = 0;
+                            s = 0;
+                            break;
+                    }
+                    if (stop == false) break;
+                }
+            }            
         }
         public void Update()//매 프레임마다 반복해야하는 일.
         {//1.입력을 받음
@@ -39,15 +66,34 @@ namespace GameFrameWork
             //컴퓨터의 사양마다 다른 진행을 없애기 위해
             //*Time.deltaTime 을 해주면 프레임 차이가 나도 초당 게임의 진행이 같다.
             //프레임 간격시간을 곱해주는 순간 초당 행동이 되는 것이다.
-            playTime += 3.0f * Time.deltaTime;
+            //ex) playTime += 3.0f * Time.deltaTime;
             //매초마다 1초 2초 3초가 표시되게하기
             //누적된 시간을 저장하는 변수 필요.
             //선생님코드
             playTime += Time.deltaTime;
-            if(playTime>=1.0)
+            //playTime *= 2;
+
+            Console.SetCursorPosition(0, 1); //(행, 열)해당 위치로 이동해서 출력
+            Console.Write(h.ToString("00:"));
+            Console.Write(m.ToString("00:"));
+            Console.Write((s).ToString("00\n"));
+            
+            if (playTime>=1.0)
             {
+                //Console.Clear();
                 playTime = 0.0f;
-                Console.WriteLine($"{++Count}초");
+                ++s;
+                if (s > 59)
+                {
+                    s = 0;
+                    ++m;
+                }
+                if (m > 59)
+                {
+                    m = 0;
+                    ++h;                    
+                }
+                //Console.WriteLine(s.ToString("00"));//"00"형태로 출력하라는 포맷 제공
             }
             //sec_check += Time.deltaTime;
             /*
@@ -60,7 +106,7 @@ namespace GameFrameWork
             */
         //3.계산결과 그림
             Draw();
-            //위의 1,2,3 논리 순서 매우중요!! 
+            //위의 1,2,3 논리 순서 중요!! 
         }
         void Draw()
         {
@@ -79,9 +125,10 @@ namespace GameFrameWork
          //게임은 실시간으로 그림을 그리며 연속적으로 보여준다.
          //일반적으로 이벤트가 일어났을 때 동작하도록 Frame Work를 만든다.
          //하지만 게임은 계속 돌아가야한다.유저가 아무행동도 안하거나, 인터넷이 끊겼더라도.
-
+            //실습
             //타이머를 만들자
-            //스페이스바를 누르면 시간이 증가하고 다시 스페이스바를 누르면 멈춘다.
+            //스페이스바를 누르면 시간이 증가하고 다시 스페이스바를 누르면 멈춘다. 엔터를 누르면 초기화.
+            // 00:00:00 시:분:초
             //1 frame work 시간을 재야함.
             GameFrameWork gfw = new GameFrameWork();
             long startTick = DateTime.Now.Ticks;
